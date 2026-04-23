223270fbda2fb464bac449dee16d0a96258ddea3
jihanrhmdn <jihanrhmdn24@gmail.com>
2026-04-23 21:01:16 +0700

    jurnal sementara

---
+++ Modul 1/Soal_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int Beras, Telur, MinyakGoreng;
+    cout << "Masukkan Harga Beras ";
+    cin >> Beras; 
+    cout << "Masukkan Harga Telur ";
+    cin >> Telur;
+    cout << "Masukkan Harga Minyak Goreng ";
+    cin >> MinyakGoreng; 
+    cout << "Totaluang ";
+
+    // Total Belanja 
+    int Totalbelanja (Beras+Telur+MinyakGoreng);
+    // Pajak
+    int Pajak (Total + 25%);
+    // Kembalian 
+    int Kembalian 
+
+    
+    
+    cout << "================================= " << endl;
+    cout << "|        STRUK KEMBALIAN        | " << endl;
+    cout << "================================= " << endl;
+    cout << "[Bina]: Nih, Than. Rincian kembaliannya: " << endl;
+    cout << "Total Kembalian : Rp " << endl;
+    cout << "---------------------------------------" <<  endl;
+    cout << "Rp 100.000 : " << endl;
+    cout << "Rp 50.000  : " << endl;
+    cout << "Rp 20.000  : " << endl;
+    cout << "Rp 10.000  : " << endl;
+    cout << "Rp 5.000   : " << endl;
+    cout << "Rp 1.000   : " << endl;
+    cout << "Rp 500     : " << endl;
+    cout << "Rp 200     : " << endl;
+    cout << "Rp 100     : " << endl;
+    --------------------------------------------
+
+    
+    return 0;
+}
---
+++ Modul 1/tes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int Beras, Telur, MinyakGoreng;
+    cout << "Masukkan Harga Beras ";
+    cin >> Beras; 
+    cout << "Masukkan Harga Telur ";
+    cin >> Telur;
+    cout << "Masukkan Harga Minyak Goreng ";
+    cin >> MinyakGoreng;
+
+    return 0;
+}
